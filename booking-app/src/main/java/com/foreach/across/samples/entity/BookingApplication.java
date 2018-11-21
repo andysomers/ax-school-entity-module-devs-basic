796adbcea6514abf32c79e7653959a7514dbf5bb
Arne Vandamme <arne@foreach.be>
2018-11-21 18:12:45 +0100

    - Remove unused files - Add test user authentication configuration

--- booking-app/src/main/java/com/foreach/across/samples/entity/BookingApplication.java
+++ booking-app/src/main/java/com/foreach/across/samples/entity/BookingApplication.java
@@ -1,6 +1,8 @@
 package com.foreach.across.samples.entity;
 
 import com.foreach.across.config.AcrossApplication;
+import com.foreach.across.modules.entity.EntityModule;
+import com.foreach.across.modules.hibernate.jpa.AcrossHibernateJpaModule;
 import com.foreach.across.modules.web.AcrossWebModule;
 import org.springframework.boot.SpringApplication;
 
@@ -8,7 +10,9 @@ import java.util.Collections;
 
 @AcrossApplication(
 		modules = {
-				AcrossWebModule.NAME
+				AcrossWebModule.NAME,
+				AcrossHibernateJpaModule.NAME,
+				EntityModule.NAME
 		}
 )
 public class BookingApplication
--- booking-app/src/main/java/com/foreach/across/samples/entity/application/domain/booking/Booking.java
+++ booking-app/src/main/java/com/foreach/across/samples/entity/application/domain/booking/Booking.java
@@ -1,8 +1,35 @@
 package com.foreach.across.samples.entity.application.domain.booking;
 
+import com.foreach.across.modules.hibernate.business.EntityWithDto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.Persistable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * A single show booking.
  */
-public class Booking
+@Entity
+@Builder(toBuilder = true)
+@Getter
+@Setter
+public class Booking implements Persistable<Long>, EntityWithDto<Booking>
 {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Override
+	public Booking toDto() {
+		return toBuilder().build();
+	}
+
+	@Override
+	public boolean isNew() {
+		return id == null;
+	}
 }
