d74a25285b27e585c7f95afb2408134cbdafcd51
Arne Vandamme <arne@foreach.be>
2018-11-21 17:32:08 +0100

    - fix poms - add code style

--- booking-app/src/main/java/com/foreach/across/samples/entity/BookingApplication.java
+++ booking-app/src/main/java/com/foreach/across/samples/entity/BookingApplication.java
@@ -1,14 +1,11 @@
 package com.foreach.across.samples.entity;
 
 import com.foreach.across.config.AcrossApplication;
-import com.foreach.across.modules.web.AcrossWebModule;
+import com.foreach.across.modules.applicationinfo.ApplicationInfoModule;
 import com.foreach.across.modules.debugweb.DebugWebModule;
 import com.foreach.across.modules.logging.LoggingModule;
-import com.foreach.across.modules.applicationinfo.ApplicationInfoModule;
+import com.foreach.across.modules.web.AcrossWebModule;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Import;
 
 import java.util.Collections;
 
