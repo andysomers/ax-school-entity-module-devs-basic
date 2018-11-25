e3c490bf030a31131efb79305ced0b75db3ec400
Steven Gentens <stg@foreach.be>
2018-11-25 15:40:37 +0100

    Completed tests for musical client - musical api returns notfound when fetching a musical that does not exist

--- booking-app/src/test/java/com/foreach/across/samples/booking/application/domain/musical/TestMusicalClient.java
+++ booking-app/src/test/java/com/foreach/across/samples/booking/application/domain/musical/TestMusicalClient.java
@@ -2,6 +2,7 @@ package com.foreach.across.samples.booking.application.domain.musical;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Before;
@@ -12,84 +13,105 @@ import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestClientException;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.assertj.core.api.Java6Assertions.assertThatThrownBy;
 
 @RunWith(SpringRunner.class)
 @TestPropertySource(properties = "musicalService.url=http://localhost:8099")
 @ContextConfiguration
 @Slf4j
-public class TestMusicalClient {
-    private MusicalClient musicalClient;
+public class TestMusicalClient
+{
+	private final ObjectMapper mapper = new ObjectMapper();
 
-    @Autowired
-    private Environment environment;
+	@Autowired
+	private Environment environment;
 
-    private Musical existingMusical;
+	private MusicalClient musicalClient;
 
-    @Before
-    public void setup() {
-        musicalClient = new MusicalClient(environment.getProperty("musicalService.url"));
+	private Musical existingMusical;
 
-        existingMusical = musicalClient.createMusical(
-                Musical.builder()
-                        .id(new MusicalId(UUID.randomUUID().toString()))
-                        .name("Winter wonderland")
-                        .description("Winterland description")
-                        .build()
-        );
-    }
+	@Before
+	public void setup() {
+		musicalClient = new MusicalClient( environment.getProperty( "musicalService.url" ) );
 
-    @After
-    public void cleanUp() {
-        musicalClient.deleteMusical(existingMusical.getId());
-    }
+		existingMusical = musicalClient.createMusical(
+				Musical.builder()
+				       .id( new MusicalId( UUID.randomUUID().toString() ) )
+				       .name( "Winter wonderland" )
+				       .description( "Winterland description" )
+				       .build()
+		);
+	}
 
-    @Test
-    public void getAllMusicals() throws JsonProcessingException {
-        List<Musical> musicals = musicalClient.getAllMusicals();
-        ObjectMapper mapper = new ObjectMapper();
+	@After
+	public void cleanUp(){
+		musicalClient.deleteMusical( existingMusical.getId() );
+	}
 
-        LOG.debug(mapper.writeValueAsString(musicals));
-    }
+	@Test
+	public void getAllMusicals() throws JsonProcessingException {
+		List<Musical> musicals = musicalClient.getAllMusicals();
+		assertThat( musicals ).isNotEmpty();
+		Musical existing = musicals.stream()
+		                           .filter( musical -> Objects.equals( musical.getId(), existingMusical.getId() ) )
+		                           .findFirst().orElse( null );
+		assertForExistingMusical( existing );
+		LOG.debug( mapper.writeValueAsString( musicals ) );
+	}
 
-    @Test
-    public void getMusical() throws JsonProcessingException {
-        Musical musical = musicalClient.getMusical(existingMusical.getId());
-        ObjectMapper mapper = new ObjectMapper();
+	@Test
+	public void getMusical() throws JsonProcessingException {
+		Musical musical = musicalClient.getMusical( existingMusical.getId() );
+		assertForExistingMusical( musical );
+		LOG.debug( mapper.writeValueAsString( musical ) );
+	}
 
-        LOG.debug(mapper.writeValueAsString(musical));
-    }
+	@Test
+	public void createAndDeleteMusical() throws JsonProcessingException {
+		Musical musicalDto = Musical.builder()
+		                            .name( "New musical" )
+		                            .description( "New musical description" )
+		                            .build();
+		Musical musical = musicalClient.createMusical( musicalDto );
+		assertForMusical( musical, musicalDto.getName(), musicalDto.getDescription() );
+		LOG.debug( mapper.writeValueAsString( musical ) );
 
-    @Test
-    public void createMusical() throws JsonProcessingException {
-        Musical musicalDto = Musical.builder()
-                .name("New musical")
-                .description("New musical description")
-                .build();
-        Musical musical = musicalClient.createMusical(musicalDto);
-        ObjectMapper mapper = new ObjectMapper();
+		musicalClient.deleteMusical( musical.getId() );
+		assertThatThrownBy( () -> musicalClient.getMusical( musical.getId() ) )
+				.isInstanceOf( RestClientException.class );
+	}
 
-        LOG.debug(mapper.writeValueAsString(musical));
-        musicalClient.deleteMusical(musical.getId());
-    }
+	@Test
+	public void updateMusical() throws JsonProcessingException {
+		Musical musicalDto = Musical.builder()
+		                            .id( existingMusical.getId() )
+		                            .name( "Updated musical" )
+		                            .description( "Newly updated description" )
+		                            .build();
+		Musical musical = musicalClient.updateMusical( musicalDto );
+		assertThat( musical )
+				.hasFieldOrPropertyWithValue( "id", musicalDto.getId() );
+		assertForMusical( musical, musicalDto.getName(), musicalDto.getDescription() );
+		LOG.debug( mapper.writeValueAsString( musical ) );
+	}
 
-    @Test
-    public void updateMusical() throws JsonProcessingException {
-        Musical musicalDto = Musical.builder()
-                .id(existingMusical.getId())
-                .name("Updated musical")
-                .description("Newly updated description")
-                .build();
-        Musical musical = musicalClient.updateMusical(musicalDto);
-        ObjectMapper mapper = new ObjectMapper();
+	private void assertForExistingMusical( Musical musical ) {
+		assertForMusical( musical, existingMusical.getName(), existingMusical.getDescription() );
+		assertThat( musical )
+				.hasFieldOrPropertyWithValue( "id", existingMusical.getId() );
+	}
 
-        LOG.debug(mapper.writeValueAsString(musical));
-    }
+	private void assertForMusical( Musical musical, String name, String description ) {
+		assertThat( musical )
+				.hasFieldOrPropertyWithValue( "name", name )
+				.hasFieldOrPropertyWithValue( "description", description );
+	}
 
-    @Test
-    public void deleteMusical() throws JsonProcessingException {
-        musicalClient.deleteMusical(existingMusical.getId());
-    }
 }
